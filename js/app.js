33f5f82e9632873499475d006fc9fd173b4bf321
shym2501 <sulthannurya@gmail.com>
2021-05-25 05:29:50 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(
+    function(registration) {
+      console.log("Berhasil registrasi service worker dengan scope:", registration.scope);
+    }).catch(
+    function(err) {
+      console.log('registrasi gagal')
+    })
+}
+
 function SW(args) {
   this.button = $(args.button);
   this.toast = $(args.toast);
